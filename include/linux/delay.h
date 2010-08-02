0bda087d2f1e11f4a3fed3a1bc948e618eed7fdf
Takashi Iwai <tiwai@suse.de>
2010-08-02 08:50:25 +0200

    Add ssleep()

--- include/linux/delay.h
+++ include/linux/delay.h
@@ -4,6 +4,7 @@
 #define mdelay(x) do { } while (0)
 #define udelay(x) do { } while (0)
 #define msleep(x) do { } while (0)
+#define ssleep(x) do { } while (0)
 #define HZ	1000
 #define msecs_to_jiffies(x)	(x)
 
