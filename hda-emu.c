51e1feb27a237281f0fccc43d856e8a8d48995cf
Raymond Yau <superquad.vortex2@gmail.com>
2011-04-24 17:47:45 +0800

    hda-emu - Check channels count for playback/capture using PCM command
    Signed-off-by: Raymond Yau <superquad.vortex2@gmail.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- hda-emu.c
+++ hda-emu.c
@@ -563,6 +563,10 @@ void hda_test_pcm(int id, int subid,
 			hda_log(HDA_LOG_INFO, " %d", rate_consts[i]);
 	}
 	hda_log(HDA_LOG_INFO, "\n");
+	if ((channels < runtime->hw.channels_min) ||
+	    (channels > runtime->hw.channels_max))
+		hda_log(HDA_LOG_ERR, "Channels count (%d) not available for %s\n", 
+			channels, (dir ? "capture" : "playback"));	
 	hda_log(HDA_LOG_INFO, "Prepare PCM, rate=%d, channels=%d, "
 		"format=%d bits\n",
 		rate, channels, format);
