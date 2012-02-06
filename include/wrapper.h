0553926050f99453750c7d8e6c1a4e5d8799d7e2
Takashi Iwai <tiwai@suse.de>
2012-02-06 15:14:48 +0100

    Add definitions of INT_MAX & co

--- include/wrapper.h
+++ include/wrapper.h
@@ -89,6 +89,19 @@ typedef uint8_t __u8;
 typedef int8_t s8;
 typedef int8_t __s8;
 
+#define USHRT_MAX	((u16)(~0U))
+#define SHRT_MAX	((s16)(USHRT_MAX>>1))
+#define SHRT_MIN	((s16)(-SHRT_MAX - 1))
+#define INT_MAX		((int)(~0U>>1))
+#define INT_MIN		(-INT_MAX - 1)
+#define UINT_MAX	(~0U)
+#define LONG_MAX	((long)(~0UL>>1))
+#define LONG_MIN	(-LONG_MAX - 1)
+#define ULONG_MAX	(~0UL)
+#define LLONG_MAX	((long long)(~0ULL>>1))
+#define LLONG_MIN	(-LLONG_MAX - 1)
+#define ULLONG_MAX	(~0ULL)
+
 typedef _Bool bool;
 #define true 1
 #define false 0
