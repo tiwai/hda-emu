aa481a7015111e53539680130bf32db7aea473e7
Takashi Iwai <tiwai@suse.de>
2013-11-12 10:43:00 +0100

    Add round_up() and round_down() macros

--- include/wrapper.h
+++ include/wrapper.h
@@ -154,6 +154,10 @@ typedef int pm_message_t;
 
 #define put_user(a,b)	({*(b) = (a); 0;})
 
+#define __round_mask(x, y) ((__typeof__(x))((y)-1))
+#define round_up(x, y) ((((x)-1) | __round_mask(x, y))+1)
+#define round_down(x, y) ((x) & ~__round_mask(x, y))
+
 #define jiffies		0
 static inline bool time_after_eq(unsigned long a, unsigned long b)
 { return 1; }
