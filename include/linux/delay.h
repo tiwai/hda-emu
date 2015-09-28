8b333b2651d04db046a693eac25b609bf175d720
Takashi Iwai <tiwai@suse.de>
2015-09-28 14:31:53 +0200

    Add usleep_range() wrapper
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/linux/delay.h
+++ include/linux/delay.h
@@ -5,6 +5,7 @@ static inline void mdelay(unsigned long x) {}
 static inline void udelay(unsigned long x) {}
 static inline void msleep(unsigned long x) {}
 static inline void ssleep(unsigned long x) {}
+static inline void usleep_range(unsigned long min, unsigned long max) {}
 #define HZ	1000
 #define msecs_to_jiffies(x)	(x)
 
