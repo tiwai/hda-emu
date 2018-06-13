b1362c3b6916b10596e422de251591cee88ee8ef
Takashi Iwai <tiwai@suse.de>
2018-06-13 11:48:35 +0200

    Define some dummy I/O functions for patch_ca0132.c
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -29,4 +29,22 @@ static inline int pci_read_config_word(struct pci_dev *dev, int where, u16 *val)
 	return 0;
 }
 
+/*
+ * FIXME: dummy functions for ca0132
+ */
+
+static inline void writeb(unsigned char val, void *addr) {}
+static inline unsigned char readb(void *addr) { return 0; }
+static inline void writew(unsigned short val, void *addr) {}
+static inline unsigned short readw(void *addr)  { return 0; }
+static inline void writel(unsigned int val, void *addr) {}
+static inline unsigned int readl(void *addr) { return 0; }
+
+static inline void *pci_iomap(struct pci_dev *pci, int bar, int offset)
+{
+	return (void *)1UL;
+}
+
+static inline void iounmap(void *addr) {}
+
 #endif /* __LINUX_PCI_H */
