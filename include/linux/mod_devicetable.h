25312b3b71886dc9354d2f4934499258e5663994
Takashi Iwai <tiwai@suse.de>
2019-04-20 08:55:46 +0200

    Add UUID definitions
    Just to pass the compilation.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/linux/mod_devicetable.h
+++ include/linux/mod_devicetable.h
@@ -1,1 +1,10 @@
+#ifndef UUID_STRING_LEN
+#define	UUID_STRING_LEN		36
+#define UUID_SIZE 16
+
+typedef struct {
+	__u8 b[UUID_SIZE];
+} uuid_t;
+#endif
+
 #include "../../dist/include/linux/mod_devicetable.h"
